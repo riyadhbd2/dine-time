bd52b1b81a806fb2927d3e71314dbf99b366a2a3
Easir Arafat <riyadhbd2@Easirs-MacBook-Air.local>
2025-07-05 10:38:41 +0200

    initial setup done

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-5xl text-center text-primary mt-3 font-bold font-quicksand-bold">
+        Welcome to App
+      </Text>
+    </View>
+  );
+}
