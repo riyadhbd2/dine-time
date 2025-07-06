3aef65cdd3f9248f291b1296d91ca61f8e036c78
Easir Arafat <riyadhbd2@Easirs-MacBook-Air.local>
2025-07-06 10:54:42 +0200

    home and cart added

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,83 @@
-import { Text, View } from "react-native";
+import CartButton from "@/components/CartButton";
+import { images, offers } from "@/constants";
+import cn from "clsx";
+import React from "react";
+import {
+  FlatList,
+  Image,
+  Pressable,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text className="text-5xl text-center text-primary mt-3 font-bold font-quicksand-bold">
-        Welcome to App
-      </Text>
-    </View>
+    <SafeAreaView className="flex-1 bg-white">
+      <FlatList
+        data={offers}
+        renderItem={({ item, index }) => {
+          const isEven: boolean = index % 2 === 0;
+          return (
+            <View className="">
+              <Pressable
+                className={cn(
+                  "offer-card",
+                  isEven ? "flex-row-reverse" : "flex-row"
+                )}
+                style={{ backgroundColor: item.color }}
+                android_ripple={{ color: "#fffff22" }}
+              >
+                {({ pressed }) => (
+                  <React.Fragment>
+                    <View className={"h-full w-1/2"}>
+                      <Image
+                        source={item.image}
+                        className={"size-full"}
+                        resizeMode={"contain"}
+                      />
+                    </View>
+                    <View
+                      className={cn(
+                        "offer-card_info",
+                        isEven ? "pl-10" : "pr-10"
+                      )}
+                    >
+                      <Text className="text-2xl font-semibold text-white leading-tight">
+                        {item.title}
+                      </Text>
+                      <Image
+                        source={images.arrowRight}
+                        className="size-10"
+                        resizeMode="contain"
+                        tintColor="#ffffff"
+                      />
+                    </View>
+                  </React.Fragment>
+                )}
+              </Pressable>
+            </View>
+          );
+        }}
+        contentContainerClassName="pb-28 px-5"
+        ListHeaderComponent={() => (
+          <View className="flex-between flex-row w-full my-5 px-5">
+            <View className="flex-start">
+              <Text className="small-bold text-primary">DELIVER TO</Text>
+              <TouchableOpacity className="felx-center flex-row gap-x-1 mt-0.5">
+                <Text className="paragraph-bold text-dark-100">Croatia</Text>
+                <Image
+                  source={images.arrowDown}
+                  className="size-3"
+                  resizeMode="contain"
+                />
+              </TouchableOpacity>
+            </View>
+            <CartButton/>
+          </View>
+        )}
+      />
+    </SafeAreaView>
   );
 }
